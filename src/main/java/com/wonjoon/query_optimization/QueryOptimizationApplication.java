58f4532428543ccda6e8f83872936eba8d83d248
724thomas <724thomas@gmail.com>
2024-10-08 16:08:27 +0900

    inital commit

---
+++ src/main/java/com/wonjoon/query_optimization/QueryOptimizationApplication.java
@@ -0,0 +1,12 @@
+package com.wonjoon.query_optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryOptimizationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wonjoon/query_optimization/QueryOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjoon.query_optimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
