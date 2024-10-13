468f82137c4f892a81b3f4ad14a5e87b055eb0f7
724thomas <724thomas@gmail.com>
2024-10-13 21:32:23 +0900

    Settings

--- src/main/java/com/wonjoon/query_optimization/config/CacheConfig.java
+++ src/main/java/com/wonjoon/query_optimization/config/CacheConfig.java
@@ -1,9 +1,34 @@
 package com.wonjoon.query_optimization.config;
 
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
 
 @Configuration
-@EnableCaching
 public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisSerializer<Object> serializer = new GenericJackson2JsonRedisSerializer();
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(serializer))
+                .entryTtl(Duration.ofMinutes(1000));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
 }
